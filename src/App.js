3b04109b1df07a42c3b71ece50bfda1f1b6cc174
HM-Talha <92864820+HM-Talha@users.noreply.github.com>
2022-02-24 10:42:01 -0800

    upload Image

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Home } from "./Pages";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import ProjectTwo from "./Components/projecttwo";
 import Page from "./Pages/Page/index"
+import EditBottle from "./Pages/EditBottle/index"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/page" element={<Page />} />
+          <Route exact path="/editbottle" element={<EditBottle />} />
         </Routes>
       </Router>
     </div>
